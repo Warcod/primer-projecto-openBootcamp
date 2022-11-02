b73568399f14aae9cdcbb8d8506e1b84406564dd
guidoiri <guidoirigoyen@gmail.com>
2022-11-02 11:36:31 +0100

    segundo ejecricio

---
+++ second-exercise/list.js
@@ -0,0 +1,14 @@
+const cumpleaños = Date(1998, 0, 9);
+
+const nueva_lista = [
+    "guido",
+    24,
+    false,
+    cumpleaños,
+    {
+        titulo: 1984,
+        autor: "George Orwell",
+        fecha: "1989-12-09",
+        url: 'https://wikipedia.com/1984-book' 
+    }
+];
