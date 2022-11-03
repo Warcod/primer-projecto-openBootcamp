c48e7bde2fc2baf18ba8f6d9ad5b2936b84e407f
guidoiri <guidoirigoyen@gmail.com>
2022-11-03 19:29:11 +0100

    tercer ejecicio

---
+++ third-exersice/factorial-break.js
@@ -0,0 +1,14 @@
+function name(num) {
+    if (num == 0 || num == 1) {
+        let result = 1;
+        return result;
+    };
+    
+    for (let index = num - 1; index <= 1; index--) {
+        num = num * i;
+    };
+
+    return num;
+
+};
+
---
+++ third-exersice/factorial-whilw.js
@@ -0,0 +1,14 @@
+function factorice(num) {
+var result = num;
+
+    if (num == 0 || num == 1) {
+        return 1;
+    }
+
+    while (num > 1) {
+        num--;
+        result = result * num;
+    }
+
+    return result;
+}
